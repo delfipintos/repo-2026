dae84435eb86e4004c8cda6a46577e4ac94817e1
delfipintos <oyehada@gmail.com>
2026-07-01 17:00:38 -0300

    script.js

--- practica-17/js/script.js
+++ practica-17/js/script.js
@@ -1,1 +1,8 @@
+let precio = 1000;     
+let descuento = 20;      
 
+let precioFinal = precio - (precio * descuento / 100);
+
+console.log("Precio sin descuento es $ " + precio);
+console.log("El descuento es " + descuento + " %");
+console.log("Precio con descuento es $ " + precioFinal);
