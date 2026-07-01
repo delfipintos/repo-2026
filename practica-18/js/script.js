ef070f79ff51913f18f5e8cc22ae130fc3c0135a
delfipintos <oyehada@gmail.com>
2026-07-01 19:25:28 -0300

    script.js

--- practica-18/js/script.js
+++ practica-18/js/script.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     alert("Al aceptar cambia todo");
 
-    document.getElementById('main-title').textContent = "maggots for brain";
-    document.getElementById('main-title').style.color = "rgb(32, 18, 29);";
+    document.getElementById('title').textContent = "maggots for brain";
+    document.getElementById('title').style.color = "rgb(32, 18, 29);";
     
     const parrafos = document.querySelectorAll('.text-p');
     parrafos[0].textContent = "but that's just the thing that happens";
