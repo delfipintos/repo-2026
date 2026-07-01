ab917acacac060fe44784a58b3b834bc19e4be54
delfipintos <oyehada@gmail.com>
2026-07-01 19:35:15 -0300

    script.js

--- practica-18/js/script.js
+++ practica-18/js/script.js
@@ -6,9 +6,9 @@ window.onload = function() {
     
     const parrafos = document.querySelectorAll('.text-p');
     parrafos[0].textContent = "but that's just the thing that happens";
-    parrafos[0].style.color = "rgb(32, 18, 29))";
+    parrafos[0].style.color = "rgb(32, 18, 29)";
     parrafos[1].textContent = "when my baby goes away";
-    parrafos[1].style.color = "rgb(32, 18, 29))";
+    parrafos[1].style.color = "rgb(32, 18, 29)";
     
     document.body.style.backgroundColor = "rgb(90, 55, 81)";
 };
