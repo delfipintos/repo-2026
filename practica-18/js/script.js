182a69eb3a691f48cf42ace8feff97ba7feca708
delfipintos <oyehada@gmail.com>
2026-07-01 19:27:43 -0300

    script.js

--- practica-18/js/script.js
+++ practica-18/js/script.js
@@ -2,11 +2,11 @@ window.onload = function() {
     alert("Al aceptar cambia todo");
 
     document.getElementById('title').textContent = "maggots for brain";
-    document.getElementById('title').style.color = "rgb(32, 18, 29);";
+    document.getElementById('title').style.color = "rgb(32, 18, 29)";
     
     const parrafos = document.querySelectorAll('.text-p');
     parrafos[0].textContent = "but that's just the thing that happens";
     parrafos[1].textContent = "when my baby goes away";
     
-    document.body.style.backgroundColor = "rgb(90, 55, 81);";
+    document.body.style.backgroundColor = "rgb(90, 55, 81)";
 };
