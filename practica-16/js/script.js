7242c1eac6c6d3076ed8cd777c341065801853fa
delfipintos <oyehada@gmail.com>
2026-07-01 16:12:54 -0300

    script.js

--- practica-16/js/script.js
+++ practica-16/js/script.js
@@ -10,3 +10,14 @@ const universidad = "Universidad Nacional de La Matanza";
 console.log(nombreCompleto);
 console.log(edad);
 console.log(universidad);
+
+nombre = "soobin";
+apellido = "choi";
+nombreCompleto = "choi soobin";
+edad = 25;
+
+universidad = "Universidad Nacional de La Plata"; 
+
+console.log(nombreCompleto);
+console.log(edad);
+console.log(universidad);
