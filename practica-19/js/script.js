32a059102fb49ee9e5d47f9b0660b9e28f3d98dd
delfipintos <oyehada@gmail.com>
2026-07-01 21:00:37 -0300

    script.js

--- practica-19/js/script.js
+++ practica-19/js/script.js
@@ -2,7 +2,13 @@ window.onload = function() {
 let pagina = document.querySelector('body');
 if (pagina) { 
   pagina.addEventListener('click', function(event) {
-  event.target.style.color = 'red';
-});  
+
+    if (event.target.tagName === 'IMG') {
+                event.target.style.display = 'none';
+      else {
+                event.target.style.color = 'red';
 }
+            
+        });
+    }
 };
